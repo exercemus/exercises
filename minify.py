039689fe56fcee07236f0e85da3432a6d0f3473b
Gregory Conrad <gregorysconrad@gmail.com>
2022-08-01 12:31:35 -0400

    Minified the new changes

--- minify.py
+++ minify.py
@@ -13,4 +13,10 @@ def write_compressed(filename, data):
 
 if __name__ == '__main__':
     data = read_data('exercises.json')
+
+    # TODO once all exercises are merged, remove this
+    to_merge = data['exercises_to_merge']
+    del data['exercises_to_merge']
+    data['exercises'].extend(to_merge)
+
     write_compressed('exercises-minified.json', data)
